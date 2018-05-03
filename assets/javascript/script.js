995a3c216201f9f5505ae4d70aa5e2086b1f5e44
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-05-02 23:11:35 -0400

    started building out the main function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,15 +13,13 @@ var computerChoice = computerRandom();
 // End of Variable
 // ---
 // using jquery to display the random number
-$('#numRan').text(computerChoice);
+$('#numRan').text(computerChoice); // ranom number to guess
 //
-$('#winsDiv').text(wins);
-$('#lossesDiv').text(losses);
+$('#winsDiv').text(wins); // wins
+$('#lossesDiv').text(losses); // loses
 //
-$('#myPoints').text(totalScore);
-//
-document.getElementById("user-score").innerHTML = "Total Score: " + "<br>" + totalScore;
-document.getElementById("totals").innerHTML = "Wins: " + wins + " " + "Losses: " + losses;
+$('#myPoints').text(totalScore); //usernumber
+
 
 
 // random() function will pick a random number for the dog icon
@@ -32,7 +30,38 @@ function random() {
 function computerRandom() {
   return Math.floor(Math.random() * (120 - 19 + 1)) + 19;
 }
+// main function
+function myFunction(doggyNum){
+  // var numberHolder = 0;
+  totalScore += doggyNum;
+  // console.log('click');
+  // console.log(doggyNum);
+  console.log('new total is ' + totalScore);
+  return totalScore;
+}
+
 //when page loads
 $(document).ready(function(){
+//
+  $("#dog1").on("click", function() {
+    console.log(dogChoice1);
+    myFunction(dogChoice1);
+  });
+  //
+  $("#dog2").on("click", function() {
+    console.log(dogChoice2);
+      myFunction(dogChoice2);
+  });
+  //
+  $("#dog3").on("click", function() {
+    console.log(dogChoice3);
+      myFunction(dogChoice3);
+  });
+  //
+  $("#dog4").on("click", function() {
+    console.log(dogChoice4);
+      myFunction(dogChoice4);
+  });
 
-}
+});
+
